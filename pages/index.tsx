8bd79981ab18f1347abd0859959f80a779b94514
SUBHAM KUNWAR <68282784+poisendev91@users.noreply.github.com>
2023-02-16 16:14:50 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,162 @@
+import { ConnectWallet, useAddress, useContract, useContractRead, useContractWrite, Web3Button } from "@thirdweb-dev/react";
+import { useState, useEffect } from 'react'
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import Link from 'next/link'
+
+
+const DaoAddress = "0xDDe908187320A1a0B37A09786180527052902b4b"
+const Home: NextPage = () => {
+
+
+    const address = useAddress();
+    const { contract } = useContract(DaoAddress);
+
+    const [addressToApprove, setAddressToApprove] = useState("")
+    const [proposalId, setProposalId] = useState("1")
+
+    const { data: AppliedAddresses,  } = useContractRead(contract, "AppliedAddresses",address)
+    const { data: approvedUsers, } = useContractRead(contract, "approvedUsers",address)
+  
+    console.log(AppliedAddresses,address)
+  
+    const myFunction = () => {
+
+        if (address) {
+
+            if (address == "0xbe257fC43bAFc6af01d8C4001a73BF3F0853d0a4") {
+                return (
+                    <>
+                        <div className={styles.card}>
+                            <p>
+                                <label htmlFor="addressToApprove">Address To Approve :</label>
+                                <input className={styles.card1} type="text" value={addressToApprove} id="addressToApprove" name="addressToApprove" placeholder="Address to include in DAO" onChange={e => setAddressToApprove(e.target.value)} required />
+                            </p>
+                            <Web3Button
+                                contractAddress={DaoAddress}
+                                action={(contract) => {
+                                    contract.call("approveForDAO", addressToApprove)
+                                }}
+                            >
+                                Approve Now
+                            </Web3Button>
+                        </div>
+                        <div className={styles.card}>
+                            <p>
+                                <label htmlFor="proposalId">Proposal To Close :</label>
+                                <input className={styles.card1} type="text" id="proposalId" value={proposalId} name="proposalId" placeholder="ID Of Proposal" onChange={e => setProposalId(e.target.value)} required />
+                            </p>
+                            <Web3Button
+                                contractAddress={DaoAddress}
+                                action={(contract) => {
+                                    contract.call("closeVoting", proposalId)
+                                }}
+                            >
+                                Close Proposal
+                            </Web3Button>
+                        </div>
+                        <Link href="/proposal">
+                            <div className={styles.card} >
+                                <h2>Proposals &rarr;</h2>
+                                <p>
+                                    Vote on proposal or create Own Proposal now.
+                                </p>
+                            </div>
+                        </Link>
+                    </>
+                )
+            }
+            else {
+
+                if (AppliedAddresses || approvedUsers) {
+                    return (
+                    <>
+                            <p>
+                                You already applied for the DAO membership   </p>
+                            <Link href="/proposal">
+                                <div className={styles.card} >
+                                    <h2>Proposals &rarr;</h2>
+                                    <p>
+                                        Vote on proposal or create Own Proposal now.
+                                    </p>
+                                </div>
+                            </Link>
+                        </>
+                        )
+                }
+                return (
+                    <>
+
+                        <Web3Button
+                            contractAddress={DaoAddress}
+                            action={(contract) => {
+                                contract.call("applyForDAO")
+                            }}
+
+                        >
+                            Apply For DAO
+                        </Web3Button>
+                        <p>
+                           Connect you wallet with mumbai testnet to apply for DAO membership;
+                        </p>
+
+                    </>
+                )
+            }
+        }
+
+
+        else {
+            return (
+                <>
+                    <h2>Connecting... </h2>
+                    <p>
+                        Connect you wallet with mumbai testnet now.
+                    </p>
+                </>
+            )
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h2 className={styles.title}>
+                    E-DAO
+                </h2>
+                <Link href="/">
+                    <div className={styles.card} >
+                       
+                        <p>
+                            Home
+                        </p>
+                    </div>
+                </Link>
+
+                <p className={styles.description}>
+                    Get started by connecting your wallet
+                </p>
+
+                <div className={styles.connect}>
+                    <ConnectWallet />
+                </div>
+
+                <div className={styles.grid}>
+                    <div className={styles.grid}>
+
+                         {myFunction()}
+                    </div>
+
+                  
+
+
+
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
+
---
+++ pages/proposal.tsx
@@ -0,0 +1,156 @@
+import { ConnectWallet, useAddress, useContract, useContractRead, useContractWrite, Web3Button } from "@thirdweb-dev/react";
+import { useState, useEffect } from 'react'
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import Link from 'next/link'
+
+const DaoAddress = "0xDDe908187320A1a0B37A09786180527052902b4b"
+
+const proposal: NextPage = () => {
+
+    const { contract } = useContract(DaoAddress);
+    const address = useAddress();
+
+    const [proposal, setProposal] = useState("")
+    const [desc, setDesc] = useState("")
+    const [proposalId, setProposalId] = useState(1)
+    const [status, setStatus] = useState("")
+
+    const { data: approvedUser } = useContractRead(contract, "approvedUsers", address)
+    const totalProposals= useContractRead(contract, "totalProposals")
+    const { data: allProposalData, isLoading } = useContractRead(contract, "allProposals", proposalId)
+    const { mutateAsync: voteForProposal, } = useContractWrite(contract, "voteForProposal")
+
+
+    let proposalCount, proposalName, proposalDesc, proposalOwner, yesVote, noVote,proposalFinished
+
+    if (allProposalData == undefined) {
+        console.log("loading")
+    }
+    else {
+     
+        proposalCount = allProposalData[0]?.toNumber()
+        proposalName = allProposalData[1]
+        proposalDesc = allProposalData[2]
+        proposalOwner = allProposalData[3].substring(0, 12)
+        yesVote = allProposalData[4]?.toNumber()
+        noVote = allProposalData[5]?.toNumber()
+        proposalFinished = allProposalData[6]?.toString()
+    }
+
+    console.log(proposalCount, totalProposals)
+    
+    console.log(proposalOwner)
+    console.log(proposalFinished)
+
+
+    const vote = async (_vote:any) => {
+        try {
+            const data = await voteForProposal([proposalId, _vote]);
+            console.info("contract call successs", data);
+        } catch (err) {
+            console.error("contract call failure", err.reason);
+            setStatus(err.reason)
+        }
+    }
+
+
+    const myFunction = () => {
+
+       
+        return (
+
+        
+                    <> <p>
+                        <label htmlFor="proposal">HeadLines: </label>
+                        <input className={styles.card1} type="text" value={proposal} name="proposal" placeholder="Topic of Proposal" onChange={e => setProposal(e.target.value)} required/>
+                    </p> <p>
+                            <label htmlFor="description">Description: </label>
+                            <input className={styles.card1} type="text" value={desc} name="description" placeholder="Desc of Proposal" onChange={e => setDesc(e.target.value)} required />
+                    </p>
+                        <Web3Button
+                            className={styles.buttonmy}
+                            contractAddress={DaoAddress}
+                            action={(contract) => {
+                                contract.call("createProposal", proposal,desc)
+                            }}
+                        >
+                            New Proposal
+                        </Web3Button> 
+                    </>
+                )}
+
+    
+
+  
+    return (
+
+
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <div className={styles.grid}>
+                    <Link href="/">
+                        <div className={styles.card} >
+
+                            <p>
+                                Home
+                            </p>
+                        </div>
+                    </Link>
+                    <div className={styles.card}>
+                        <ConnectWallet />
+                    </div>
+
+                 </div>
+               
+                <div className={styles.card}>
+                    <h2 className={styles.title1}>
+                        Create Proposal
+                    </h2>
+                    {myFunction()}
+
+                </div>
+                <div>
+                
+                <div className={styles.grid}>
+                     
+                    <div className={styles.card}>
+                            <div className={styles.card}>
+                                <h3 className={styles.title1}>
+                                    Vote on Any Proposal By id
+
+                                </h3>
+                                <label htmlFor="proposalId">Choose Id to vote: </label>
+                                <input className={styles.card1} type="number" value={proposalId} name="proposalid" placeholder="Proposal ID" onChange={e => setProposalId(e.target.value)} required />
+                                {status}
+                            </div>
+                        <h2 className={styles.title1}>
+                                {proposalCount}: {proposalName}
+                            </h2>
+                            <p style={{ fontSize: "1rem" }}>Description: {proposalDesc} </p>
+                     <div className={styles.grid}>
+                                <button onClick={() => vote(1)} className={styles.buttongreen}>
+                                    Yes {yesVote}
+                            </button>
+                                <button onClick={() => vote(0)} className={styles.buttonred}>
+                                    No  {noVote}
+                            </button>
+                       </div>
+                        <p>
+                                Proposed By: {proposalOwner}....
+                            </p>
+                        <p>
+                                 Finished:{proposalFinished}
+                        </p>
+                    </div>
+                 
+
+                    </div>
+                      
+                    </div>
+            </main>
+        </div>
+    );
+};
+
+export default proposal;
